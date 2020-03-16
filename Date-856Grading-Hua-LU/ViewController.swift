78c7f9620620822e94779f7b1eb60e41184f71c0
helloconey <48701450+helloconey@users.noreply.github.com>
2020-03-16 13:54:37 -0700

    Initial Commit

---
+++ Date-856Grading-Hua-LU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Date-856Grading-Hua-LU
+//
+//  Created by Coney Lu on 3/16/20.
+//  Copyright © 2020 Hua Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
